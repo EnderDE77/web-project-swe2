b9ddc73cf8dfce6a7f990bfaebe3efb475914729
EraMulla <105673596+EraMulla@users.noreply.github.com>
2023-06-04 23:12:09 +0200

    Update clientview.php

--- WEB_PROJECT1/CLIENT/clientview.php
+++ WEB_PROJECT1/CLIENT/clientview.php
@@ -5,11 +5,38 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="client-view.css">
-
+    
+<style>
+    aside {
+      width: 250px;
+      padding: 20px;
+      background-color: #f5f5f5;
+    }
+    
+    #cart-items {
+      list-style-type: none;
+      padding: 0;
+    }
+    
+    #cart-total {
+      font-weight: bold;
+    }
+  </style>
 </head>
 <body>
 
-
+<main>
+    <!-- Product listing -->
+  </main>
+  
+  <aside>
+    <h2>Your Cart</h2>
+    <ul id="cart-items">
+      <!-- Cart items will be dynamically added here -->
+    </ul>
+    <p>Total: <span id="cart-total">$0</span></p>
+  </aside>
+    
 <section class="products" id="products">
     <h1 class="heading">Our <span>products</span></h1>
     <?php require_once "./product.controller.php";?>
