68ce18c59b40314b3faed6d9d516587e25c5c331
Bossman123555 <123903291+Bossman123555@users.noreply.github.com>
2023-06-06 21:17:45 +0200

    Update product.adder.php

--- WEB_PROJECT1/CLIENT/product.adder.php
+++ WEB_PROJECT1/CLIENT/product.adder.php
@@ -1,26 +1,30 @@
-<?php
 require_once "./product.model.php";
 session_start();
 $prod = [];
 $keys = [];
 $quants = [];
 $find = FALSE;
-foreach($_POST as $key => $value){
-    if(gettype($key) == "integer"){
+
+foreach ($_POST as $key => $value) {
+    if (is_numeric($key)) { // Check if $key is a number
         array_push($keys, $key);
         $find = TRUE;
-    }
-    else{
-        if($value == ''){}
+    } else {
+        if ($value == '') {
+            continue; // Skip empty values
+        }
+
         $value = (int)$value;
-        if($value == 0){}
-        else if($find == TRUE){
-        array_push($quants, $value);
+        if ($value == 0) {
+            continue; // Skip zero values
+        } elseif ($find == TRUE) {
+            array_push($quants, $value);
         }
         $find = FALSE;
     }
 }
-for($i = 0;$i<count($keys);$i++){
+
+for ($i = 0; $i < count($keys); $i++) {
     setProductToCart($mySQL, $keys[$i], $quants[$i], $_SESSION['cart_id']);
     lowerProductQuantity($connection, $keys[$i], $quants[$i]);
     modifyPaymentPrice($connection, $_SESSION['payment_id'], $keys[$i], $quants[$i]);
