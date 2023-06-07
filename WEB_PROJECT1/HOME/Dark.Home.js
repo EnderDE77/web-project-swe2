4611eaa7a3de9742091823d664825bf7e948199a
Bossman123555 <123903291+Bossman123555@users.noreply.github.com>
2023-06-07 23:45:19 +0200

    Update Dark.Home.js

--- WEB_PROJECT1/HOME/Dark.Home.js
+++ WEB_PROJECT1/HOME/Dark.Home.js
@@ -1,4 +1,8 @@
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     document.documentElement.classList.add('dark-mode');
   }
-  
+const toggleButton = document.getElementById('dark-mode-toggle');
+
+toggleButton.addEventListener('click', () => {
+  document.documentElement.classList.toggle('dark-mode');
+});
