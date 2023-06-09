42f4bcf6c559eb332bca362693c529844d3b0856
Bossman123555 <123903291+Bossman123555@users.noreply.github.com>
2023-06-09 09:36:44 +0200

    Update index.php

--- WEB_PROJECT1/HOME/index.php
+++ WEB_PROJECT1/HOME/index.php
@@ -130,6 +130,16 @@
         window.location.href="LogIn.html";
     }
 </script>
+    <script>
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.documentElement.classList.add('dark-mode');
+  }
+const toggleButton = document.getElementById('dark-mode-toggle');
+
+toggleButton.addEventListener('click', () => {
+  document.documentElement.classList.toggle('dark-mode');
+});
+    </script>
 
 
 </body>
